8886e4ad0af239371b71ddbd3e6c7362b7d78a77
Dilnaz <dilnaz@stanford.edu>
2023-01-31 15:57:11 -0800

    blackjack

---
+++ blackjack.py
@@ -0,0 +1,22 @@
+import random
+import numpy as np
+
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def hit_card(current):
+    return card = random.choice(cards)
+
+first_card = random.choice(cards)
+second_card = random.choice(cards)
+
+def count_score(first, second):
+
+score = first_card + second_card
+
+while (score <= 17):
+    if count_score(first_card, second_card)
+
+
+
+    
